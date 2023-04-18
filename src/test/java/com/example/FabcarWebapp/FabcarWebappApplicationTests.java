2c6ff95c17493e5bdc4b0682d6f4392c165dda70
shkimhi <ajfqhksh1@gmail.com>
2023-04-18 14:48:11 +0900

    fabcar webapp ex

---
+++ src/test/java/com/example/FabcarWebapp/FabcarWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FabcarWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabcarWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
